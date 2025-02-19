3e3e587f34f1f4d6c0922a0b7881d676846f1170
LuanLucasGon <lucasluangon@gmail.com>
2025-02-19 16:27:07 -0300

    refactor: adding entity atributtes on truck class

--- src/main/java/com/example/teste_agro_e/domain/Truck.java
+++ src/main/java/com/example/teste_agro_e/domain/Truck.java
@@ -1,16 +1,23 @@
 package com.example.teste_agro_e.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "trucks")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Truck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, unique = true)
     private String licensePlate;
     private String brand;
     private String model;
--- src/main/java/com/example/teste_agro_e/repositories/TruckRepository.java
+++ src/main/java/com/example/teste_agro_e/repositories/TruckRepository.java
@@ -3,5 +3,5 @@ package com.example.teste_agro_e.repositories;
 import com.example.teste_agro_e.domain.Truck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrcukRepository extends JpaRepository<Truck, Long> {
+public interface TruckRepository extends JpaRepository<Truck, Long> {
 }
