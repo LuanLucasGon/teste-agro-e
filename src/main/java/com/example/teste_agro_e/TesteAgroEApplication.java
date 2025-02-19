a0eed1d51fdbe0d6d6eceb8b9bcd5814752f2cc5
LuanLucasGon <lucasluangon@gmail.com>
2025-02-19 15:56:52 -0300

    first commit

---
+++ src/main/java/com/example/teste_agro_e/TesteAgroEApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_agro_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAgroEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAgroEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste_agro_e/TesteAgroEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste_agro_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAgroEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
