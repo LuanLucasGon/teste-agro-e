d72ad6edbe8ecea6b2982133f3d581594a2f270d
LuanLucasGon <lucasluangon@gmail.com>
2025-02-19 16:20:29 -0300

    feat: creating truck repository

---
+++ src/main/java/com/example/teste_agro_e/repositories/TrcukRepository.java
@@ -0,0 +1,7 @@
+package com.example.teste_agro_e.repositories;
+
+import com.example.teste_agro_e.domain.Truck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrcukRepository extends JpaRepository<Truck, Long> {
+}
